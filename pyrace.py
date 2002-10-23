e75fd5d8b6f012080bfdf6cd8a86c3890fcb98ab
balint <>
2002-10-23 12:44:55 +0000

    Added status line; count and display score.

--- pyrace.py
+++ pyrace.py
@@ -2,18 +2,19 @@
 
 # TODO:
 #
-# score line; score for time
-# high score
+# reliable speed (even if key pressed)
+# high score; nice status line
 # fuel; fuel pickup
 # config file
 # nice splash, Game Over
 # slippery oil
 # colors
-# reliable speed (even if key pressed)
 # gas, break (or link break to steering)
+# high score table
 
 import curses
 import random
+import string
 
 TICK = 1
 CAR = "^"
@@ -30,11 +31,13 @@ ESC = 27
 
 class Race:
 
-    def __init__(self, race_win):
-        self.race_win = race_win
+    def __init__(self, main_win):
+        self.race_win = main_win.derwin(1, 0)
         self.race_win.keypad(1)
         self.race_win.scrollok(1)
-        (self.height, self.width) = race_win.getmaxyx()
+        (self.height, self.width) = self.race_win.getmaxyx()
+        self.status_line = StatusLine(main_win.derwin(1, self.width, 0, 0))
+        self.score = 0
         self.cary = self.height-1
         self.carx = self.width/2
         self.rx_max = (self.width-len(RSLICE))
@@ -47,6 +50,7 @@ class Race:
 
     def main_loop(self):
         while not self.crash and not self.esc:
+            self.score += 1
             self.key(self.race_win.getch())
             self.next_rslice()
             self.next_obs()
@@ -82,13 +86,33 @@ class Race:
             self.race_win.addstr(0, self.rx+len(EDGE)+self.ox, OBS)
         self.crash = self.race_win.inch(self.cary, self.carx)!=ord(ROAD)
         self.race_win.addstr(self.cary, self.carx, CAR)
+        self.status_line.update_score(self.score)
+        self.status_line.refresh()
+
+class StatusLine:
+
+    SCORE_LABEL = "Score: "
+    SCORE_WIDTH = 5
+    
+    def __init__(self, status_win):
+        self.status_win = status_win
+        self.width = status_win.getmaxyx()[1]
+        self.score_pos = self.width-StatusLine.SCORE_WIDTH-1
+        self.status_win.addstr(0, self.score_pos-len(StatusLine.SCORE_LABEL),
+                               StatusLine.SCORE_LABEL)
+        
+    def update_score(self, score):
+        self.status_win.addstr(0, self.score_pos,
+                               string.zfill(score, StatusLine.SCORE_WIDTH))
+
+    def refresh(self):
+        self.status_win.refresh()
         
 class Game:
 
     def __init__(self, main_win):
         self.main_win = main_win
-        race_win = main_win.derwin(1, 0)
-        self.race = Race(race_win)
+        self.race = Race(main_win)
 
     def main_loop(self):
         self.race.main_loop()
